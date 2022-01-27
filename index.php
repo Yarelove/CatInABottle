65a422725e7cdeb16f980d037ec4f0bce4c51fa5
Yarelove <95253341+Yarelove@users.noreply.github.com>
2022-01-27 15:20:31 +0300

    some add

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+	<title>SCUM PAGE</title>
+
+	<body>
+		<div class="first-block"></div>
+			<h1>Hello mom</h1>
+			<h2>Hi son</h2>
+			<h3>lets make brother?</h3>
+			<h4>go</h4>
+			<form action="test.html">
+				<button><h5>*sex*</h5></button>
+			</form>
+		</div>
+		<div class="second-block"></div>
+			<h1>The End.</h1>
+		</div>
+	</body>
+	<style>
+		form button
+		{
+			width: 60px;
+    		height: 60px;
+			border-radius:100%;
+		}
+	</style>
+</html>
